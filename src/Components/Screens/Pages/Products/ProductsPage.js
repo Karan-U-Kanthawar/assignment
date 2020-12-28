a43f1c2ed50a3bca274c7197cea61e412a266525
Karan <karankanthawar@gmail.com>
2020-12-28 22:34:44 +0530

    changed hero animation

--- src/Components/Screens/Pages/Products/ProductsPage.js
+++ src/Components/Screens/Pages/Products/ProductsPage.js
@@ -10,14 +10,15 @@ export const ProductsPage = () => {
         <Col lg={4}>
           <h2 className='heading'>Products</h2>
           <p className='content'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            nec erat vel lectus bibendum vehicula quis vitae enim. Integer
-            tincidunt.
+            We are building a suite of user firendly crypto products for the
+            normal retail investor who wants to invest in crypto but is
+            intimidated by its complexity. We see a lack of an integrated
+            ecosystem that simplifies investing without taking away from all
+            that crypto has to offer.
           </p>
           <p className='content'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            nec erat vel lectus bibendum vehicula quis vitae enim. Integer
-            tincidunt.
+            Click on the graphic to understand all the legos that make up
+            Cryption Network
           </p>
         </Col>
         <Col lg={8}>
